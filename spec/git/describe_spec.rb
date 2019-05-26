085245d7f2702060c8a812b7de53e1cfd69c7519
Nicola Bernardini <nicola.bernardini.rome@gmail.com>
2019-05-26 10:51:46 +0200

    added skipped test

--- spec/git/describe_spec.rb
+++ spec/git/describe_spec.rb
@@ -12,4 +12,15 @@ RSpec.describe Git::Describe do
     expect(Git::Describe.git).to match(/^v[0-9]+\.[0-9]+.*\-[a-z0-9]+/)
   end
 
+  it 'will return an empty string if it is not inside a git repository' do
+    #
+    # this is actually quite tricky to be testes especially in continuous
+    # integration environments like travis-ci & the like. (It requires to be
+    # tested +outside+ of a repository but the code is actually in a
+    # repository, so the only trick could be to change to an upper directory and
+    # check, but this is tricky in a chrooted environment)
+    #
+    skip('This cannot be easily tested with regression tests')
+  end
+
 end
